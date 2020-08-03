d3af59e54911a1a466b2492142acd25b62081149
olejodd10 <67640511+olejodd10@users.noreply.github.com>
2020-08-03 20:15:11 +0200

    Update sitatside.mjs
    Refresher nå siden, siden QUOTEJSONID må få ny verdi

--- sitatside.mjs
+++ sitatside.mjs
@@ -8,25 +8,28 @@ import { Quote } from "./modules/quote.mjs";
 let accessToken; //Brukes av appendJsonQuote under, må være global
 const quoteContainer = document.querySelector("#quoteContainer"); //Brukes av onAccept, er tydeligere å ha den her oppe
 
-async function onAccept() { //OBS! Gjør async hvis du vil awaite noe
+function onAccept() { //OBS! Gjør async hvis du vil awaite noe
 	accessToken = gapi.auth.getToken().access_token; 
 	if (localStorage.getItem("QUOTEJSONID") === null) {
-		await QGDI.makeJsonQuoteFile(PRJCTFLDRID, "sitater.json", accessToken)
+		QGDI.makeJsonQuoteFile(PRJCTFLDRID, "sitater.json", accessToken)
 		.then(id => {
 			localStorage.setItem("QUOTEJSONID", id);
+			console.log("Refreshing page.");
+			location.reload();
 		})
 		.catch(err => {
 			console.error(err);
 			alertError(err, "makeJsonQuoteFile in sitatside.html onAccept");
 			console.log("Resetter PRJCTFLDRID.");
 			localStorage.removeItem("PRJCTFLDRID");
+		})
+	} else {
+		QDOMI.displayJsonQuotes(quoteContainer, QUOTEJSONID, accessToken)
+		.catch(err => {
+			console.error(err);
+			alertError(err, "displayJsonQuotes in sitatside.html onAccept");
 		});
 	}
-	QDOMI.displayJsonQuotes(quoteContainer, QUOTEJSONID, accessToken)
-	.catch(err => {
-		console.error(err);
-		alertError(err, "displayJsonQuotes in sitatside.html onAccept");
-	});
 }
 
 function onDenial() {
